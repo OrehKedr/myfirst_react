59b7c7ab7c6a8cac7b50a9f4a465d0393d7caa3c
Alexandr Prokofyev <alprokoviev@gmail.com>
2019-11-26 22:51:13 +0400

    Refactoring using jsx syntax

---
+++ static_src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const element = <h1 className="element">It seems we connected React</h1>
+
+ReactDOM.render(
+    element,
+    document.getElementById('root'),
+);
