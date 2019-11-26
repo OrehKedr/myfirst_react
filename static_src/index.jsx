1ca7bc9d5ec2c8cd395f382cb24063bc98b9429e
Alexandr Prokofyev <alprokoviev@gmail.com>
2019-11-27 00:37:37 +0400

    Added the ability to send messages by clicking on the button

--- static_src/index.jsx
+++ static_src/index.jsx
@@ -3,13 +3,27 @@ import ReactDOM from 'react-dom';
 
 let messages = ['Привет', 'Как дела?'];
 
+let handleClick = () => {
+    messages.push('Нормально');
+    
+    ReactDOM.render(
+        <MessageField messages = { messages }/>,
+        document.getElementById('root'),
+    );
+}
+
 const MessageComponent = (props) => <div>{ props.text }</div>;
 
 const MessageField = (props) => {
-    return props.messages.map(message => <MessageComponent text = { message } />);
-}
-const Element = (props) => {
-    return <h1 className="element">{ props.content }</h1>;
+    const messageElements = props.messages.map(message => <MessageComponent text = { message } />);
+
+    return (
+        <div>
+            <h1>React Chat</h1>
+            { messageElements }
+            <button onClick = { handleClick }>Push me!</button>
+        </div>
+    );        
 }
 
 ReactDOM.render(
