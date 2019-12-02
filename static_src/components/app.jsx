9a13fe8e981c881144b49260906b662371b12ab8
Alexandr Prokofyev <alprokoviev@gmail.com>
2019-12-02 20:35:04 +0400

    Наш первый React-компонент

---
+++ static_src/components/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class App extends React.Component {
+    state = {
+        text: 'Наш первый React-компонент'
+    };
+
+    render() {
+        return (
+            <h1>{this.state.text}</h1>
+        );
+    };
+}
