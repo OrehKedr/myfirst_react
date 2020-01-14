7a7e109af445d80f421a713e8c3bbdec426aa0fb
Alexandr Prokofyev <alprokoviev@gmail.com>
2020-01-14 22:30:52 +0300

    Подключение и настройка redux-persist

--- static_src/index.jsx
+++ static_src/index.jsx
@@ -2,17 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Router from './components/Router';
 import { ConnectedRouter } from 'connected-react-router';
+import { PersistGate } from 'redux-persist/integration/react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import { Provider } from 'react-redux';
 import initStore, { history } from './utils/store';
 
+const {store, persistor } = initStore();
+
 ReactDOM.render(
-    <Provider store={ initStore() }>
-        <ConnectedRouter history={ history }>
-            <MuiThemeProvider>
-                <Router />
-            </MuiThemeProvider>
-        </ConnectedRouter>
+    <Provider store={ store }>
+        <PersistGate loading={ null } persistor={ persistor }>
+            <ConnectedRouter history={ history }>
+                <MuiThemeProvider>
+                    <Router />
+                </MuiThemeProvider>
+            </ConnectedRouter>
+        </PersistGate>
     </Provider>,
     document.getElementById('root'),
 );
